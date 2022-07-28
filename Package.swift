fead2a498ff82e675e55788b3deb502efe0baf3f
randymcmillan <randy.lee.mcmillan@gmail.com>
2022-07-28 00:20:13 -0400

    Package.swift: add secp256k1.swift to dependencies

--- Package.swift
+++ Package.swift
@@ -1,3 +1,4 @@
 dependencies: [
-    .Package(url: "https://github.com/daltoniam/Starscream.git", majorVersion: 4)
+    .Package(url: "https://github.com/daltoniam/Starscream.git", majorVersion: 4),
+    .Package(url: "https://github.com/jb55/secp256k1.swift.git", branch: "main")
 ]
