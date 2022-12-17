0513f9342becfb6ea0122d6969f1440ad5f745bc
Sam DuBois <sdubois@umass.edu>
2022-12-17 10:50:29 -0700

    ui: Add more padding everywhere.
    The app felt a bit claustrophobic
    
    Changed-Changed: Added padding in more views so it feels less claustrophobic

--- damus/Views/PostButton.swift
+++ damus/Views/PostButton.swift
@@ -18,7 +18,7 @@ func PostButton(action: @escaping () -> ()) -> some View {
     })
     .background(Color.blue)
     .cornerRadius(38.5)
-    .padding()
+    .padding(.bottom)
     .shadow(color: Color.black.opacity(0.3),
             radius: 3,
             x: 3,
