5dbf8029da8ee4c90977d50ad1c8f27cc4266fbf
Joel Klabo <joelklabo@gmail.com>
2023-01-07 12:49:39 -0800

    Update Shaka Assets and Use for EventActionBar

--- damus/Views/EventActionBar.swift
+++ damus/Views/EventActionBar.swift
@@ -168,12 +168,8 @@ struct LikeButton: View {
     
     var body: some View {
         Button(action: action) {
-            if liked {
-                Text("🤙", comment: "Button with emoji to like an event.")
-            } else {
-                Text("🤙")
-                    .grayscale(1)
-            }
+            Image(liked ? "shaka-full" : "shaka-line")
+                .foregroundColor(liked ? .orange : .gray)
         }
     }
 }
