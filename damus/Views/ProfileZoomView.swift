e4ad15ced1dc9f66db8b9de32bbb765f72527b21
Joel Klabo <joelklabo@gmail.com>
2023-01-20 14:22:49 -0800

    Center PFP in Zoom View
    Closes: #357
    Changlog-Fixed: Center profile picture in zoom view

--- damus/Views/ProfileZoomView.swift
+++ damus/Views/ProfileZoomView.swift
@@ -65,9 +65,9 @@ struct ProfileZoomView: View {
             .zIndex(1)
 
             VStack(alignment: .center) {
-                Spacer()
-                    .frame(height: 120)
 
+                Spacer()
+                
                 ProfilePicView(pubkey: pubkey, size: 200.0, highlight: .none, profiles: profiles)
                     .padding(100)
                     .scaledToFit()
@@ -78,6 +78,8 @@ struct ProfileZoomView: View {
                     .modifier(SwipeToDismissModifier(minDistance: nil, onDismiss: {
                         presentationMode.wrappedValue.dismiss()
                     }))
+                
+                Spacer()
 
             }
         }
