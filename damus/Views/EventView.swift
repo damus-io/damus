7e1daf78165af266940e5d5d2e42b913045600b2
William Casarin <jb55@jb55.com>
2023-01-23 10:38:53 -0800

    refactor: move Highlight to its own file

--- damus/Views/EventView.swift
+++ damus/Views/EventView.swift
@@ -8,34 +8,6 @@
 import Foundation
 import SwiftUI
 
-enum Highlight {
-    case none
-    case main
-    case reply
-    case custom(Color, Float)
-
-    var is_main: Bool {
-        if case .main = self {
-            return true
-        }
-        return false
-    }
-
-    var is_none: Bool {
-        if case .none = self {
-            return true
-        }
-        return false
-    }
-
-    var is_replied_to: Bool {
-        switch self {
-        case .reply: return true
-        default: return false
-        }
-    }
-}
-
 enum EventViewKind {
     case small
     case normal
