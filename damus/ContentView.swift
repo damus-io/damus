0902548336507250a89d77b24e4d0295c9af878b
radixrat <radixrat@protonmail.com>
2023-02-02 13:31:24 -0500

    Clicking on relay numbers on home view brings you to config
    Changelog-Changed: Clicking relay numbers now goes to relay config
    Closes: #491

--- damus/ContentView.swift
+++ damus/ContentView.swift
@@ -260,9 +260,11 @@ struct ContentView: View {
                                     ToolbarItem(placement: .navigationBarTrailing) {
                                         HStack(alignment: .center) {
                                             if home.signal.signal != home.signal.max_signal {
-                                                Text("\(home.signal.signal)/\(home.signal.max_signal)", comment: "Fraction of how many of the user's relay servers that are operational.")
-                                                    .font(.callout)
-                                                    .foregroundColor(.gray)
+                                                NavigationLink(destination: RelayConfigView(state: damus_state!)) {
+                                                    Text("\(home.signal.signal)/\(home.signal.max_signal)", comment: "Fraction of how many of the user's relay servers that are operational.")
+                                                        .font(.callout)
+                                                        .foregroundColor(.gray)
+                                                }
                                             }
 
                                         }
