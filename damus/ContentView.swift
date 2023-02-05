29a82065868e1c72fe12b19f8d2ac8e7f345a964
William Casarin <jb55@jb55.com>
2023-02-05 10:49:18 -0800

    Hide blocked users from search results
    Changelog-Fixed: Hide blocked users from search results

--- damus/ContentView.swift
+++ damus/ContentView.swift
@@ -197,7 +197,7 @@ struct ContentView: View {
     var MaybeSearchView: some View {
         Group {
             if let search = self.active_search {
-                SearchView(appstate: damus_state!, search: SearchModel(pool: damus_state!.pool, search: search))
+                SearchView(appstate: damus_state!, search: SearchModel(contacts: damus_state!.contacts, pool: damus_state!.pool, search: search))
             } else {
                 EmptyView()
             }
