dcd06066cd1e35563f17dd5620a30507349f675c
ericholguin <eric.holguinsanchez@gmail.com>
2023-02-09 23:52:32 -0700

    undo extraneous files

--- damus/Views/BlurView.swift
+++
@@ -1,8 +0,0 @@
-//
-//  BlurView.swift
-//  damus
-//
-//  Created by eric on 2/9/23.
-//
-
-import Foundation
--- damus/Views/StickyHeader.swift
+++
@@ -1,8 +0,0 @@
-//
-//  StickyHeader.swift
-//  damus
-//
-//  Created by eric on 2/9/23.
-//
-
-import Foundation
