7674f425961ac46bc3d96309840e96348cfa2ed4
William Casarin <jb55@jb55.com>
2023-03-27 12:26:30 -0400

    Damus Colors Helper

---
+++ damus/Components/DamusColors.swift
@@ -0,0 +1,21 @@
+//
+//  DamusColors.swift
+//  damus
+//
+//  Created by William Casarin on 2023-03-27.
+//
+
+import Foundation
+import SwiftUI
+
+class DamusColors {
+    static let adaptableGray = Color("DamusAdaptableGrey")
+    static let white = Color("DamusWhite")
+    static let black = Color("DamusBlack")
+    static let lightGray = Color("DamusLightGray")
+    static let mediumGray = Color("DamusMediumGray")
+    static let darkGray = Color("DamusDarkGray")
+    static let green = Color("DamusGreen")
+    static let purple = Color("DamusGreen")
+}
+
