040e45213240a707c2b1bdf5c06a41395401bbc7
William Casarin <jb55@jb55.com>
2023-04-21 10:01:32 -0700

    Fix having to set onlyzaps mode every time on restart
    Changelog-Fixed: Fix having to set onlyzaps mode every time on restart

--- damus/Models/UserSettingsStore.swift
+++ damus/Models/UserSettingsStore.swift
@@ -302,7 +302,7 @@ class UserSettingsStore: ObservableObject {
         disable_animation = should_disable_image_animation()
         auto_translate = UserDefaults.standard.object(forKey: "auto_translate") as? Bool ?? true
         show_only_preferred_languages = UserDefaults.standard.object(forKey: "show_only_preferred_languages") as? Bool ?? false
-        onlyzaps_mode = UserDefaults.standard.object(forKey: "hide_reactions") as? Bool ?? false
+        onlyzaps_mode = UserDefaults.standard.object(forKey: "onlyzaps_mode") as? Bool ?? false
 
         // Note from @tyiu:
         // Default translation service is disabled by default for now until we gain some confidence that it is working well in production.
