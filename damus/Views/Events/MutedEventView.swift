6b1bb9bece8c0164d41b238bc09b1658d86d9b3b
Blake Jakopovic <wakoinc@gmail.com>
2023-04-22 11:14:16 +0200

    Updated blocked phrasing to muted

--- damus/Views/Events/MutedEventView.swift
+++ damus/Views/Events/MutedEventView.swift
@@ -10,37 +10,37 @@ import SwiftUI
 struct MutedEventView: View {
     let damus_state: DamusState
     let event: NostrEvent
-    
+
     let selected: Bool
     @State var shown: Bool
-    
+
     init(damus_state: DamusState, event: NostrEvent, selected: Bool) {
         self.damus_state = damus_state
         self.event = event
         self.selected = selected
         self._shown = State(initialValue: should_show_event(contacts: damus_state.contacts, ev: event))
     }
-    
+
     var should_mute: Bool {
         return !should_show_event(contacts: damus_state.contacts, ev: event)
     }
-    
+
     var MutedBox: some View {
         ZStack {
             RoundedRectangle(cornerRadius: 20)
                 .foregroundColor(DamusColors.adaptableGrey)
-            
+
             HStack {
-                Text("Post from a user you've blocked", comment: "Text to indicate that what is being shown is a post from a user who has been blocked.")
+                Text("Post from a user you've muted", comment: "Text to indicate that what is being shown is a post from a user who has been muted.")
                 Spacer()
-                Button(shown ? NSLocalizedString("Hide", comment: "Button to hide a post from a user who has been blocked.") : NSLocalizedString("Show", comment: "Button to show a post from a user who has been blocked.")) {
+                Button(shown ? NSLocalizedString("Hide", comment: "Button to hide a post from a user who has been muted.") : NSLocalizedString("Show", comment: "Button to show a post from a user who has been muted.")) {
                     shown.toggle()
                 }
             }
             .padding(10)
         }
     }
-    
+
     var Event: some View {
         Group {
             if selected {
@@ -50,7 +50,7 @@ struct MutedEventView: View {
             }
         }
     }
-    
+
     var body: some View {
         Group {
             if should_mute {
@@ -64,7 +64,7 @@ struct MutedEventView: View {
             guard let mutes = notif.object as? [String] else {
                 return
             }
-            
+
             if mutes.contains(event.pubkey) {
                 shown = false
             }
@@ -73,7 +73,7 @@ struct MutedEventView: View {
             guard let unmutes = notif.object as? [String] else {
                 return
             }
-            
+
             if unmutes.contains(event.pubkey) {
                 shown = true
             }
@@ -84,9 +84,9 @@ struct MutedEventView: View {
 struct MutedEventView_Previews: PreviewProvider {
     @State static var nav_target: NostrEvent = test_event
     @State static var navigating: Bool = false
-    
+
     static var previews: some View {
-        
+
         MutedEventView(damus_state: test_damus_state(), event: test_event, selected: false)
             .frame(width: .infinity, height: 50)
     }
