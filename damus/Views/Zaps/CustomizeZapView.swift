c4f41220e5875fbe72454319200eae98bc7c0a75
William Casarin <jb55@jb55.com>
2023-04-23 09:54:38 -0700

    refactor: extract ZapTypePicker into its own file

--- damus/Views/Zaps/CustomizeZapView.swift
+++ damus/Views/Zaps/CustomizeZapView.swift
@@ -8,13 +8,6 @@
 import SwiftUI
 import Combine
 
-enum ZapType {
-    case pub
-    case anon
-    case priv
-    case non_zap
-}
-
 struct ZapAmountItem: Identifiable, Hashable {
     let amount: Int
     let icon: String
@@ -88,85 +81,6 @@ struct CustomizeZapView: View {
         self.state = state
     }
     
-    func zap_type_desc(type: ZapType) -> String {
-        switch type {
-        case .pub:
-            return NSLocalizedString("Everyone will see that you zapped", comment: "Description of public zap type where the zap is sent publicly and identifies the user who sent it.")
-        case .anon:
-            return NSLocalizedString("No one will see that you zapped", comment: "Description of anonymous zap type where the zap is sent anonymously and does not identify the user who sent it.")
-        case .priv:
-            let pk = event.pubkey
-            let prof = state.profiles.lookup(id: pk)
-            let name = Profile.displayName(profile: prof, pubkey: pk).username
-            return String.localizedStringWithFormat(NSLocalizedString("private_zap_description", value: "Only '%@' will see that you zapped them", comment: "Description of private zap type where the zap is sent privately and does not identify the user to the public."), name)
-        case .non_zap:
-            return NSLocalizedString("No zaps will be sent, only a lightning payment.", comment: "Description of non-zap type where sats are sent to the user's wallet as a regular Lightning payment, not as a zap.")
-        }
-    }
-    
-    var ZapTypePicker: some View {
-        VStack(spacing: 20) {
-            Text("Zap type")
-                .font(.system(size: 18, weight: .heavy))
-            ZapTypeSelection(text: "Public", comment: "Picker option to indicate that a zap should be sent publicly and identify the user as who sent it.", img: "person.2.circle.fill", action: {zap_type = ZapType.pub}, type: ZapType.pub)
-            ZapTypeSelection(text: "Private", comment: "Picker option to indicate that a zap should be sent privately and not identify the user to the public.", img: "lock.circle.fill", action: {zap_type = ZapType.priv}, type: ZapType.priv)
-            ZapTypeSelection(text: "Anonymous", comment: "Picker option to indicate that a zap should be sent anonymously and not identify the user as who sent it.", img: "person.crop.circle.fill.badge.questionmark", action: {zap_type = ZapType.anon}, type: ZapType.anon)
-            ZapTypeSelection(text: "None", comment: "Picker option to indicate that sats should be sent to the user's wallet as a regular Lightning payment, not as a zap.", img: "bolt.circle.fill", action: {zap_type = ZapType.non_zap}, type: ZapType.non_zap)
-        }
-        .padding(.horizontal)
-    }
-    
-    func ZapTypeSelection(text: LocalizedStringKey, comment: StaticString, img: String, action: @escaping () -> (), type: ZapType) -> some View {
-        Button(action: action) {
-            VStack(alignment: .leading, spacing: 5) {
-                HStack {
-                    Image(systemName: img)
-                        .foregroundColor(.gray)
-                        .font(.system(size: 24))
-                    Text(text, comment: comment)
-                        .font(.system(size: 20, weight: .semibold))
-                    Spacer()
-                }
-                .padding(.horizontal)
-                Text(zap_type_desc(type: type))
-                    .padding(.horizontal)
-                    .foregroundColor(.gray)
-                    .font(.system(size: 16))
-            }
-        }
-        .frame(minWidth: 400, maxWidth: .infinity, minHeight: 50, maxHeight: 70)
-        .foregroundColor(fontColor())
-        .background(zap_type == type ? fillColor() : DamusColors.adaptableGrey)
-        .cornerRadius(15)
-        .overlay(RoundedRectangle(cornerRadius: 15)
-            .stroke(DamusColors.purple.opacity(zap_type == type ? 1.0 : 0.0), lineWidth: 2))
-    }
-    
-    func ZapTypeButton() -> some View {
-        Button(action: {
-            show_zap_types = true
-        }) {
-            switch zap_type {
-            case .pub:
-                Image(systemName: "person.2")
-                Text("Public")
-            case .anon:
-                Image(systemName: "person.fill.questionmark")
-                Text("Anonymous")
-            case .priv:
-                Image(systemName: "lock")
-                Text("Private")
-            case .non_zap:
-                Image(systemName: "bolt")
-                Text("None")
-            }
-        }
-        .font(.headline)
-        .foregroundColor(fontColor())
-        .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
-        .background(DamusColors.adaptableGrey)
-        .cornerRadius(15)
-    }
     
     func amount_parts(_ n: Int) -> [ZapAmountItem] {
         var i: Int = -1
@@ -331,6 +245,32 @@ struct CustomizeZapView: View {
                 hideKeyboard()
             }
     }
+    
+    func ZapTypeButton() -> some View {
+        Button(action: {
+            show_zap_types = true
+        }) {
+            switch zap_type {
+            case .pub:
+                Image(systemName: "person.2")
+                Text("Public")
+            case .anon:
+                Image(systemName: "person.fill.questionmark")
+                Text("Anonymous")
+            case .priv:
+                Image(systemName: "lock")
+                Text("Private")
+            case .non_zap:
+                Image(systemName: "bolt")
+                Text("None")
+            }
+        }
+        .font(.headline)
+        .foregroundColor(fontColor())
+        .padding(EdgeInsets(top: 5, leading: 15, bottom: 5, trailing: 15))
+        .background(DamusColors.adaptableGrey)
+        .cornerRadius(15)
+    }
 
     var CustomZap: some View {
         VStack(alignment: .center, spacing: 20) {
@@ -354,11 +294,11 @@ struct CustomizeZapView: View {
         }
         .sheet(isPresented: $show_zap_types) {
             if #available(iOS 16.0, *) {
-                ZapTypePicker
+                ZapTypePicker(zap_type: $zap_type, profiles: state.profiles, pubkey: event.pubkey)
                     .presentationDetents([.medium])
                     .presentationDragIndicator(.visible)
             } else {
-                ZapTypePicker
+                ZapTypePicker(zap_type: $zap_type, profiles: state.profiles, pubkey: event.pubkey)
             }
         }
     }
