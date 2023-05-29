57760f63bb92a773b8dd427b6635a3d25cfdf1b7
gladiusKatana <garthsnyder@protonmail.com>
2023-05-29 10:45:43 -0400

    add .frame & .position modifiers to TextEntry using ScrollView geometry

--- damus/Views/PostView.swift
+++ damus/Views/PostView.swift
@@ -207,13 +207,16 @@ struct PostView: View {
         }
     }
     
-    var TextEntry: some View {
+    func TextEntry(scrollViewGeometry: GeometryProxy) -> some View {
+        GeometryReader { (geometry: GeometryProxy) in
         ZStack(alignment: .topLeading) {
             TextViewWrapper(attributedText: $post, postTextViewCanScroll: $postTextViewCanScroll, cursorIndex: newCursorIndex, getFocusWordForMention: { word, range in
                 focusWordAttributes = (word, range)
                 self.newCursorIndex = nil
             })
                 .environmentObject(tagModel)
+                .frame(maxHeight: scrollViewGeometry.size.height)
+                .position(x: geometry.frame(in: .local).midX, y: scrollViewGeometry.frame(in: .local).midY)
                 .focused($focus)
                 .textInputAutocapitalization(.sentences)
                 .onChange(of: post) { p in
@@ -227,6 +230,7 @@ struct PostView: View {
                     .foregroundColor(Color(uiColor: .placeholderText))
                     .allowsHitTesting(false)
             }
+        }
         }
     }
     
@@ -297,12 +301,12 @@ struct PostView: View {
         }
     }
     
-    func Editor(deviceSize: GeometryProxy) -> some View {
+    func Editor(deviceSize: GeometryProxy, scrollViewGeometry: GeometryProxy) -> some View {
         VStack(alignment: .leading, spacing: 0) {
             HStack(alignment: .top) {
                 ProfilePicView(pubkey: damus_state.pubkey, size: PFP_SIZE, highlight: .none, profiles: damus_state.profiles, disable_animation: damus_state.settings.disable_animation)
                 
-                TextEntry
+                TextEntry(scrollViewGeometry: scrollViewGeometry)
             }
             .frame(height: deviceSize.size.height * multiply_factor)
             .id("post")
@@ -327,16 +331,19 @@ struct PostView: View {
                 TopBar
                 
                 ScrollViewReader { scroller in
+                    GeometryReader { (geometry: GeometryProxy) in
                     ScrollView {
                         if case .replying_to(let replying_to) = self.action {
                             ReplyView(replying_to: replying_to, damus: damus_state, originalReferences: $originalReferences, references: $references)
                         }
                         
-                        Editor(deviceSize: deviceSize)
+                        /// `scrollViewGeometry` is passed to TextEntry's .frame & .position modifiers
+                        Editor(deviceSize: deviceSize, scrollViewGeometry: geometry)
                     }
                     .frame(maxHeight: searching == nil ? .infinity : 70)
                     .onAppear {
                         scroll_to_event(scroller: scroller, id: "post", delay: 1.0, animate: true, anchor: .top)
+                    }
                     }
                 }
                 
