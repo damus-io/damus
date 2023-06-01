15b52f9d57ebaca99825d048c4708cc2e62756e6
Suhail Saqan <suhail.saqan@gmail.com>
2023-06-01 15:35:14 -0500

    add ChatBubbleShape shape

--- damus/Views/DMView.swift
+++ damus/Views/DMView.swift
@@ -158,6 +158,111 @@ struct DMView: View {
     }
 }
 
+struct ChatBubbleShape: Shape {
+    enum Direction {
+        case left
+        case right
+        case none
+    }
+    
+    let direction: Direction
+    
+    func path(in rect: CGRect) -> Path {
+        return (direction == .none) ? getBubblePath(in: rect) : ( (direction == .left) ? getLeftBubblePath(in: rect) : getRightBubblePath(in: rect) )
+    }
+    
+    private func getBubblePath(in rect: CGRect) -> Path {
+        let width = rect.width
+        let height = rect.height
+        let cornerRadius: CGFloat = 17
+        let path = Path { p in
+            p.move(to: CGPoint(x: cornerRadius, y: height))
+            p.addLine(to: CGPoint(x: width - cornerRadius, y: height))
+            p.addCurve(to: CGPoint(x: width, y: height - cornerRadius),
+                       control1: CGPoint(x: width - cornerRadius/2, y: height),
+                       control2: CGPoint(x: width, y: height - cornerRadius/2))
+            p.addLine(to: CGPoint(x: width, y: cornerRadius))
+            p.addCurve(to: CGPoint(x: width - cornerRadius, y: 0),
+                       control1: CGPoint(x: width, y: cornerRadius/2),
+                       control2: CGPoint(x: width - cornerRadius/2, y: 0))
+            p.addLine(to: CGPoint(x: cornerRadius, y: 0))
+            p.addCurve(to: CGPoint(x: 0, y: cornerRadius),
+                       control1: CGPoint(x: cornerRadius/2, y: 0),
+                       control2: CGPoint(x: 0, y: cornerRadius/2))
+            p.addLine(to: CGPoint(x: 0, y: height - cornerRadius))
+            p.addCurve(to: CGPoint(x: cornerRadius, y: height),
+                       control1: CGPoint(x: 0, y: height - cornerRadius/2),
+                       control2: CGPoint(x: cornerRadius/2, y: height))
+        }
+        return path
+    }
+    
+    private func getLeftBubblePath(in rect: CGRect) -> Path {
+        let width = rect.width
+        let height = rect.height
+        let path = Path { p in
+            p.move(to: CGPoint(x: 25, y: height))
+            p.addLine(to: CGPoint(x: width - 20, y: height))
+            p.addCurve(to: CGPoint(x: width, y: height - 20),
+                       control1: CGPoint(x: width - 8, y: height),
+                       control2: CGPoint(x: width, y: height - 8))
+            p.addLine(to: CGPoint(x: width, y: 20))
+            p.addCurve(to: CGPoint(x: width - 20, y: 0),
+                       control1: CGPoint(x: width, y: 8),
+                       control2: CGPoint(x: width - 8, y: 0))
+            p.addLine(to: CGPoint(x: 21, y: 0))
+            p.addCurve(to: CGPoint(x: 4, y: 20),
+                       control1: CGPoint(x: 12, y: 0),
+                       control2: CGPoint(x: 4, y: 8))
+            p.addLine(to: CGPoint(x: 4, y: height - 11))
+            p.addCurve(to: CGPoint(x: 0, y: height),
+                       control1: CGPoint(x: 4, y: height - 1),
+                       control2: CGPoint(x: 0, y: height))
+            p.addLine(to: CGPoint(x: -0.05, y: height - 0.01))
+            p.addCurve(to: CGPoint(x: 11.0, y: height - 4.0),
+                       control1: CGPoint(x: 4.0, y: height + 0.5),
+                       control2: CGPoint(x: 8, y: height - 1))
+            p.addCurve(to: CGPoint(x: 25, y: height),
+                       control1: CGPoint(x: 16, y: height),
+                       control2: CGPoint(x: 20, y: height))
+            
+        }
+        return path
+    }
+    
+    private func getRightBubblePath(in rect: CGRect) -> Path {
+        let width = rect.width
+        let height = rect.height
+        let path = Path { p in
+            p.move(to: CGPoint(x: 25, y: height))
+            p.addLine(to: CGPoint(x:  20, y: height))
+            p.addCurve(to: CGPoint(x: 0, y: height - 20),
+                       control1: CGPoint(x: 8, y: height),
+                       control2: CGPoint(x: 0, y: height - 8))
+            p.addLine(to: CGPoint(x: 0, y: 20))
+            p.addCurve(to: CGPoint(x: 20, y: 0),
+                       control1: CGPoint(x: 0, y: 8),
+                       control2: CGPoint(x: 8, y: 0))
+            p.addLine(to: CGPoint(x: width - 21, y: 0))
+            p.addCurve(to: CGPoint(x: width - 4, y: 20),
+                       control1: CGPoint(x: width - 12, y: 0),
+                       control2: CGPoint(x: width - 4, y: 8))
+            p.addLine(to: CGPoint(x: width - 4, y: height - 11))
+            p.addCurve(to: CGPoint(x: width, y: height),
+                       control1: CGPoint(x: width - 4, y: height - 1),
+                       control2: CGPoint(x: width, y: height))
+            p.addLine(to: CGPoint(x: width + 0.05, y: height - 0.01))
+            p.addCurve(to: CGPoint(x: width - 11, y: height - 4),
+                       control1: CGPoint(x: width - 4, y: height + 0.5),
+                       control2: CGPoint(x: width - 8, y: height - 1))
+            p.addCurve(to: CGPoint(x: width - 25, y: height),
+                       control1: CGPoint(x: width - 16, y: height),
+                       control2: CGPoint(x: width - 20, y: height))
+        }
+        return path
+    }
+}
+
 struct DMView_Previews: PreviewProvider {
     static var previews: some View {
         let ev = NostrEvent(content: "Hey there *buddy*, want to grab some drinks later? 🍻", keypair: test_keypair, kind: 1, tags: [])!
