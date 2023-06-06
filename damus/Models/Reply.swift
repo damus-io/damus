f6475e8d11f3632d7d5147adb7fe4d41998b902a
TheSameCat2 <thesamecat@proton.me>
2023-06-05 20:08:25 -0500

    ui: fix replied to count is incorrect [bug] #1083
    Added deduplication of npubs in event tags to prevent erroneously counting a person twice.

--- damus/Models/Reply.swift
+++ damus/Models/Reply.swift
@@ -14,19 +14,25 @@ struct ReplyDesc {
 
 func make_reply_description(_ tags: [[String]]) -> ReplyDesc {
     var c = 0
-    var ns: [String] = []
-    var i = tags.count - 1
-        
-    while i >= 0 {
-        let tag = tags[i]
-        if tag.count >= 2 && tag[0] == "p" {
-            c += 1
-            if ns.count < 2 {
-                ns.append(tag[1])
-            }
-        }
-        i -= 1
-    }
-        
-    return ReplyDesc(pubkeys: ns, others: c)
+     var ns: [String] = []
+     var i = tags.count - 1
+     var seenPubkeys = Set<String>()
+         
+     while i >= 0 {
+         let tag = tags[i]
+         if tag.count >= 2 && tag[0] == "p" {
+             if (!seenPubkeys.contains(tag[1])) {
+                 if ns.count < 2 {
+                     ns.append(tag[1])
+                 }
+                 c += 1
+             }
+             else {
+                 seenPubkeys.insert(tag[1])
+             }
+         }
+         i -= 1
+     }
+         
+     return ReplyDesc(pubkeys: ns, others: c)
 }
