7d80985b06d5efb4a1c9b0f098d674858a5a542e
William Casarin <jb55@jb55.com>
2023-06-27 05:31:14 +0200

    nozaps: remove zap button on posts

--- damus/Views/ActionBar/EventActionBar.swift
+++ damus/Views/ActionBar/EventActionBar.swift
@@ -86,7 +86,7 @@ struct EventActionBar: View {
                 }
             }
 
-            if let lnurl = self.lnurl {
+            if !damus_state.settings.nozaps, let lnurl = self.lnurl {
                 Spacer()
                 ZapButton(damus_state: damus_state, target: ZapTarget.note(id: event.id, author: event.pubkey), lnurl: lnurl, zaps: self.damus_state.events.get_cache_data(self.event.id).zaps_model)
             }
