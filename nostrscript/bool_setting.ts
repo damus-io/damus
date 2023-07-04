3d0448a92912418ca788f565bf29907c1d8639f0
William Casarin <jb55@jb55.com>
2023-07-03 17:10:57 -0700

    smaller nostrscript

--- nostrscript/bool_setting.ts
+++ nostrscript/bool_setting.ts
@@ -1,18 +1,19 @@
 
 import * as nostr from './nostr'
 
-export function go(): i32 {
-	var setting = "mny`or"
-	var new_setting = ""
-	for (let i = 0; i < setting.length; i++) {
-		new_setting += String.fromCharCode(setting.charCodeAt(i) + 1);
-	}
-	// this should fail
-	if (nostr.set_bool_setting("shmorg", true)) {
-		// you shouldn't be able to set settings that dont exist
-		return 0;
-	}
-	return nostr.set_bool_setting(new_setting, false)
+var setting = "mny`or"
+var new_setting = ""
+
+for (let i = 0; i < setting.length; i++) {
+	new_setting += String.fromCharCode(setting.charCodeAt(i) + 1);
 }
 
-go()
+// this should fail
+if (nostr.set_bool_setting("shmorg", true)) {
+	// you shouldn't be able to set settings that dont exist
+	return 0;
+}
+
+if (!nostr.set_bool_setting(new_setting, false)) {
+	return 0;
+}
