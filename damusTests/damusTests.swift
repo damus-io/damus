031c7823ae5078de13e96a504750cf69cda7874f
William Casarin <jb55@jb55.com>
2023-07-11 07:21:16 -0700

    refactor: move hashtag tests to their own file

--- damusTests/damusTests.swift
+++ damusTests/damusTests.swift
@@ -176,65 +176,6 @@ class damusTests: XCTestCase {
         XCTAssertEqual(ev.tags[1][1], "bitcoin")
         
     }
-    
-    func testParseHashtag() {
-        let parsed = parse_mentions(content: "some hashtag #bitcoin derp", tags: []).blocks
-        
-        XCTAssertNotNil(parsed)
-        XCTAssertEqual(parsed.count, 3)
-        XCTAssertEqual(parsed[0].is_text, "some hashtag ")
-        XCTAssertEqual(parsed[1].is_hashtag, "bitcoin")
-        XCTAssertEqual(parsed[2].is_text, " derp")
-    }
-    
-    func testHashtagWithComma() {
-        let parsed = parse_mentions(content: "some hashtag #bitcoin, cool", tags: []).blocks
-        
-        XCTAssertNotNil(parsed)
-        XCTAssertEqual(parsed.count, 3)
-        XCTAssertEqual(parsed[0].is_text, "some hashtag ")
-        XCTAssertEqual(parsed[1].is_hashtag, "bitcoin")
-        XCTAssertEqual(parsed[2].is_text, ", cool")
-    }
-    
-    func testHashtagWithEmoji() {
-        let parsed = parse_mentions(content: "some hashtag #bitcoin☕️ cool", tags: []).blocks
-        
-        XCTAssertNotNil(parsed)
-        XCTAssertEqual(parsed.count, 3)
-        XCTAssertEqual(parsed[0].is_text, "some hashtag ")
-        XCTAssertEqual(parsed[1].is_hashtag, "bitcoin☕️")
-        XCTAssertEqual(parsed[2].is_text, " cool")
-    }
-    
-    func testHashtagWithAccents() {
-        let parsed = parse_mentions(content: "hello from #türkiye", tags: []).blocks
-        
-        XCTAssertNotNil(parsed)
-        XCTAssertEqual(parsed.count, 2)
-        XCTAssertEqual(parsed[0].is_text, "hello from ")
-        XCTAssertEqual(parsed[1].is_hashtag, "türkiye")
-    }
-
-    func testHashtagWithNonLatinCharacters() {
-        let parsed = parse_mentions(content: "this is a #시험 hope it works", tags: []).blocks
-        
-        XCTAssertNotNil(parsed)
-        XCTAssertEqual(parsed.count, 3)
-        XCTAssertEqual(parsed[0].is_text, "this is a ")
-        XCTAssertEqual(parsed[1].is_hashtag, "시험")
-        XCTAssertEqual(parsed[2].is_text, " hope it works")
-    }
-    
-    func testParseHashtagEnd() {
-        let parsed = parse_mentions(content: "some hashtag #bitcoin", tags: []).blocks
-        
-        XCTAssertNotNil(parsed)
-        XCTAssertEqual(parsed.count, 2)
-        XCTAssertEqual(parsed[0].is_text, "some hashtag ")
-        XCTAssertEqual(parsed[1].is_hashtag, "bitcoin")
-    }
-    
     func testParseMentionOnlyText() {
         let parsed = parse_mentions(content: "there is no mention here", tags: [["e", "event_id"]]).blocks
         
