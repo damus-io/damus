damus-io/damus

commit ad6a1962bbddc17762c4a73dbe25398c7d79f861

Scroll to top of event instead of bottom
This is pretty important for longform events

Changelog-Changed: Start at top when reading longform events

damus/Views/EventDetailView.swift
@@ -21,7 +21,7 @@ struct EventDetailView_Previews: PreviewProvider {
     }
 }
 
-func scroll_to_event(scroller: ScrollViewProxy, id: String, delay: Double, animate: Bool, anchor: UnitPoint = .bottom) {
+func scroll_to_event(scroller: ScrollViewProxy, id: String, delay: Double, animate: Bool, anchor: UnitPoint = .top) {
     DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
         if animate {
             withAnimation {

commit f2cf30a72880fb85bf5268a79aca57fc20e04e62

Scroll to top for longform events only
Fixes: ad6a1962 ("Scroll to top of event instead of bottom")

damus/Views/EventDetailView.swift
@@ -21,7 +21,7 @@ struct EventDetailView_Previews: PreviewProvider {
     }
 }
 
-func scroll_to_event(scroller: ScrollViewProxy, id: String, delay: Double, animate: Bool, anchor: UnitPoint = .top) {
+func scroll_to_event(scroller: ScrollViewProxy, id: String, delay: Double, animate: Bool, anchor: UnitPoint = .bottom) {
     DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
         if animate {
             withAnimation {

damus/Views/ThreadView.swift
@@ -88,7 +88,8 @@ struct ThreadView: View {
             }.navigationBarTitle(NSLocalizedString("Thread", comment: "Navigation bar title for note thread."))
             .onAppear {
                 thread.subscribe()
-                scroll_to_event(scroller: reader, id: self.thread.event.id, delay: 0.0, animate: false)
+                let anchor: UnitPoint = self.thread.event.known_kind == .longform ? .top : .bottom
+                scroll_to_event(scroller: reader, id: self.thread.event.id, delay: 0.0, animate: false, anchor: anchor)
             }
             .onDisappear {
                 thread.unsubscribe()