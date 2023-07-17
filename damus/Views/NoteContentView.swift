a8b9eb27c3aed4cbfa2fb088f7f6ff2397f1cb93
Fishcake <me@fshck.com>
2023-07-17 10:48:25 +0900

    Include .m3u8 as video media

--- damus/Views/NoteContentView.swift
+++ damus/Views/NoteContentView.swift
@@ -623,7 +623,8 @@ func classify_url(_ url: URL) -> UrlType {
         return .media(.image(url))
     }
     
-    if str.hasSuffix(".mp4") || str.hasSuffix(".mov") {
+    if str.hasSuffix(".mp4") || str.hasSuffix(".mov") ||
+        str.hasSuffix(".m3u8") {
         return .media(.video(url))
     }
     
