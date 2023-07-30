06a66a3709d9deb69c6a0d1da1185cb5d3abeee3
William Casarin <jb55@jb55.com>
2023-07-30 10:44:14 -0700

    add some type aliases to make the ndb move more incremental

---
+++ damus/Nostr/Pubkey.swift
@@ -0,0 +1,14 @@
+//
+//  Pubkey.swift
+//  damus
+//
+//  Created by William Casarin on 2023-07-30.
+//
+
+import Foundation
+
+// prepare a more gradual transition to the ndb branch
+typealias FollowRef = ReferencedId
+typealias Pubkey = String
+typealias NoteId = String
+typealias Privkey = String
