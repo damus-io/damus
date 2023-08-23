bf3ca4a1869adc15d614c3976a62b8db3276377b
William Casarin <jb55@jb55.com>
2023-08-23 16:23:18 -0700

    status: truncate statuses to a single line
    Changelog-Fixed: Fix long status lines

--- damus/Components/Status/UserStatusView.swift
+++ damus/Components/Status/UserStatusView.swift
@@ -13,15 +13,17 @@ struct UserStatusView: View {
     @ObservedObject var status: UserStatusModel
 
     var body: some View {
-        VStack(alignment: .leading, spacing: 0) {
+        VStack(alignment: .leading, spacing: 2) {
             if let general = status.general {
                 Text(verbatim: "\(general.content)")
+                    .lineLimit(1)
                     .foregroundColor(.gray)
                     .font(.callout.italic())
             }
 
             if let playing = status.music {
                 Text(verbatim: "🎵\(playing.content)")
+                    .lineLimit(1)
                     .foregroundColor(.gray)
                     .font(.callout.italic())
             }
