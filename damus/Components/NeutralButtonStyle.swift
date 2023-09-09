94ce604b9d2a787d43227263835d412c1d31cd20
ericholguin <eric.holguinsanchez@gmail.com>
2023-09-08 21:06:31 -0600

    components: add neutral button style component

---
+++ damus/Components/NeutralButtonStyle.swift
@@ -0,0 +1,49 @@
+//
+//  NeutralButtonStyle.swift
+//  damus
+//
+//  Created by eric on 9/1/23.
+//
+
+import SwiftUI
+
+struct NeutralButtonStyle: ButtonStyle {
+    func makeBody(configuration: Self.Configuration) -> some View {
+        return configuration.label
+            .background(DamusColors.neutral1)
+            .cornerRadius(12)
+            .overlay(
+                RoundedRectangle(cornerRadius: 12)
+                    .stroke(DamusColors.neutral3, lineWidth: 1)
+            )
+            .scaleEffect(configuration.isPressed ? 0.95 : 1)
+    }
+}
+
+
+struct NeutralButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            Button(action: {
+                print("dynamic size")
+            }) {
+                Text(verbatim: "Dynamic Size")
+                    .padding()
+            }
+            .buttonStyle(NeutralButtonStyle())
+
+
+            Button(action: {
+                print("infinite width")
+            }) {
+                HStack {
+                    Text(verbatim: "Infinite Width")
+                        .padding()
+                }
+                .frame(minWidth: 300, maxWidth: .infinity, alignment: .center)
+            }
+            .buttonStyle(NeutralButtonStyle())
+            .padding()
+        }
+    }
+}
