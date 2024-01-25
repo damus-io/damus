damus-io/damus

commit af75eed83a2a1dd0eb33a0a27ded71c9f44dacbd

mention: fix broken mentions when there is text is directly after
If a user adds text right after a mention without a space, a space gets
added so the mention can be parsed correctly after posting.

Closes: https://github.com/damus-io/damus/issues/1872
Changelog-Fixed: Fix broken mentions when there is text is directly after
Lightning-url: LNURL1DP68GURN8GHJ7EM9W3SKCCNE9E3K7MF0D3H82UNVWQHKWUN9V4HXGCTHDC6RZVGR8SW3G
Signed-off-by: kernelkind <kernelkind@gmail.com>
Reviewed-by: William Casarin <jb55@jb55.com>
Signed-off-by: William Casarin <jb55@jb55.com>

damus/Views/PostView.swift
@@ -617,6 +617,13 @@ func load_draft_for_post(drafts: Drafts, action: PostAction) -> DraftArtifacts? 
 func build_post(state: DamusState, post: NSMutableAttributedString, action: PostAction, uploadedMedias: [UploadedMedia], references: [RefId]) -> NostrPost {
     post.enumerateAttributes(in: NSRange(location: 0, length: post.length), options: []) { attributes, range, stop in
         if let link = attributes[.link] as? String {
+            let nextCharIndex = range.upperBound
+            if nextCharIndex < post.length,
+               let nextChar = post.attributedSubstring(from: NSRange(location: nextCharIndex, length: 1)).string.first,
+               !nextChar.isWhitespace {
+                post.insert(NSAttributedString(string: " "), at: nextCharIndex)
+            }
+
             let normalized_link: String
             if link.hasPrefix("damus:nostr:") {
                 // Replace damus:nostr: URI prefix with nostr: since the former is for internal navigation and not meant to be posted.

damusTests/PostViewTests.swift
@@ -142,6 +142,28 @@ final class PostViewTests: XCTestCase {
         checkMentionLinkEditorHandling(content: content, replacementText: "", replacementRange: NSRange(location: 5, length: 28), shouldBeAbleToChangeAutomatically: true)
         
     }
+    
+    func testMentionLinkEditorHandling_noWhitespaceAfterLink1_addsWhitespace() {
+        var content: NSMutableAttributedString
+
+        content = NSMutableAttributedString(string: "Hello @user ")
+        content.addAttribute(.link, value: "damus:1234", range: NSRange(location: 6, length: 5))
+        checkMentionLinkEditorHandling(content: content, replacementText: "up", replacementRange: NSRange(location: 11, length: 1), shouldBeAbleToChangeAutomatically: true, expectedNewCursorIndex: 13, handleNewContent: { newManuallyEditedContent in
+            XCTAssertEqual(newManuallyEditedContent.string, "Hello @user up")
+            XCTAssertNil(newManuallyEditedContent.attribute(.link, at: 11, effectiveRange: nil))
+        })
+    }
+    
+    func testMentionLinkEditorHandling_noWhitespaceAfterLink2_addsWhitespace() {
+        var content: NSMutableAttributedString
+
+        content = NSMutableAttributedString(string: "Hello @user test")
+        content.addAttribute(.link, value: "damus:1234", range: NSRange(location: 6, length: 5))
+        checkMentionLinkEditorHandling(content: content, replacementText: "up", replacementRange: NSRange(location: 11, length: 1), shouldBeAbleToChangeAutomatically: true, expectedNewCursorIndex: 13, handleNewContent: { newManuallyEditedContent in
+            XCTAssertEqual(newManuallyEditedContent.string, "Hello @user uptest")
+            XCTAssertNil(newManuallyEditedContent.attribute(.link, at: 11, effectiveRange: nil))
+        })
+    }
 }
 
 func checkMentionLinkEditorHandling(

commit 010d71d9ed6b6bc93e158121cd0b7544e9767a8f

mentions: fix regression on char handling after mention
Previously, when a post is being sent, the next character after a
mention was checked. If it wasn't a space, a space was added. This
implementation was insufficient because there are times where a
character immediately following a mention is desired, especially for
punctuation.

This was possible in the past because the C code counts anything non
alphanumeric as being part of the mention if it starts with 'nostr:'.

The fix included follows similar logic. If the character following the
mention is non alphanumeric, it allows it. Otherwise, a space is added
so that the C code can correctly parse the mention.

A test was added which verifies non alphanumeric characters after
mentions don't get whitespace added before them.

Fixes: af75eed ("mention: fix broken mentions when there is text is directly after")
Closes: https://github.com/damus-io/damus/issues/1915
Signed-off-by: kernelkind <kernelkind@gmail.com>
Reviewed-by: William Casarin <jb55@jb55.com>
Signed-off-by: William Casarin <jb55@jb55.com>

damus/Views/PostView.swift
@@ -613,6 +613,9 @@ func load_draft_for_post(drafts: Drafts, action: PostAction) -> DraftArtifacts? 
     }
 }
 
+private func isAlphanumeric(_ char: Character) -> Bool {
+    return char.isLetter || char.isNumber
+}
 
 func build_post(state: DamusState, post: NSMutableAttributedString, action: PostAction, uploadedMedias: [UploadedMedia], references: [RefId]) -> NostrPost {
     post.enumerateAttributes(in: NSRange(location: 0, length: post.length), options: []) { attributes, range, stop in
@@ -620,7 +623,7 @@ func build_post(state: DamusState, post: NSMutableAttributedString, action: Post
             let nextCharIndex = range.upperBound
             if nextCharIndex < post.length,
                let nextChar = post.attributedSubstring(from: NSRange(location: nextCharIndex, length: 1)).string.first,
-               !nextChar.isWhitespace {
+               isAlphanumeric(nextChar) {
                 post.insert(NSAttributedString(string: " "), at: nextCharIndex)
             }
 

damusTests/PostViewTests.swift
@@ -164,6 +164,12 @@ final class PostViewTests: XCTestCase {
             XCTAssertNil(newManuallyEditedContent.attribute(.link, at: 11, effectiveRange: nil))
         })
     }
+    
+    func testMentionLinkEditorHandling_nonAlphanumericAfterLink_noWhitespaceAdded() {
+        let nonAlphaNumerics = [" ", "!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~"]
+        
+        nonAlphaNumerics.forEach { testAddingStringAfterLink(str: $0)}
+    }
 }
 
 func checkMentionLinkEditorHandling(
@@ -189,5 +195,15 @@ func checkMentionLinkEditorHandling(
         XCTAssertEqual(coordinator.textView(textView, shouldChangeTextIn: replacementRange, replacementText: replacementText), shouldBeAbleToChangeAutomatically, "Expected shouldChangeTextIn to return \(shouldBeAbleToChangeAutomatically), but was \(!shouldBeAbleToChangeAutomatically)")
 }
 
+func testAddingStringAfterLink(str: String) {
+    var content: NSMutableAttributedString
+
+    content = NSMutableAttributedString(string: "Hello @user test")
+    content.addAttribute(.link, value: "damus:1234", range: NSRange(location: 6, length: 5))
+    checkMentionLinkEditorHandling(content: content, replacementText: str, replacementRange: NSRange(location: 11, length: 0), shouldBeAbleToChangeAutomatically: false, expectedNewCursorIndex: 12, handleNewContent: { newManuallyEditedContent in
+        XCTAssertEqual(newManuallyEditedContent.string, "Hello @user" + str + " test")
+        XCTAssertNil(newManuallyEditedContent.attribute(.link, at: 11, effectiveRange: nil))
+    })
+}
 
 