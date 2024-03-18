98eddf13370e39936f55aaaf4d504b8d9c96248d
Daniel D’Aquino <daniel@daquino.me>
2024-03-18 22:21:13 +0000

    Small tweak to resolve build error
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>
    Link: 20240318222048.14226-2-daniel@daquino.me
    Signed-off-by: William Casarin <jb55@jb55.com>

--- damus/Views/Wallet/WalletView.swift
+++ damus/Views/Wallet/WalletView.swift
@@ -34,7 +34,7 @@ struct WalletView: View {
                     
                     Divider()
                     
-                    RelayView(state: damus_state, relay: nwc.relay.id, showActionButtons: .constant(false))
+                    RelayView(state: damus_state, relay: nwc.relay.id, showActionButtons: .constant(false), recommended: false)
                 }
                 .frame(maxWidth: .infinity, minHeight: 125, alignment: .top)
                 .padding(.horizontal, 10)
