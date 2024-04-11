409d1f62a8ac43ce6f0bad365989b9ef6153745c
Daniel D’Aquino <daniel@daquino.me>
2024-04-11 00:41:43 +0000

    Remove no-op performance tests that were causing issues
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

--- damusTests/damusTests.swift
+++ damusTests/damusTests.swift
@@ -29,13 +29,6 @@ class damusTests: XCTestCase {
         XCTAssertEqual(pubkey, pubkey_same)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
     func testRandomBytes() {
         let bytes = random_bytes(count: 32)
         
