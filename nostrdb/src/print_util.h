ba34b45a2675519654a0a8f83cfebf74786d9aee
William Casarin <jb55@jb55.com>
2024-04-30 23:22:30 +0200

    nostrdb: ndb: dump json in filters and fulltext queries
    This is much more useful
    
    Signed-off-by: William Casarin <jb55@jb55.com>

--- nostrdb/src/print_util.h
+++ nostrdb/src/print_util.h
@@ -1,7 +1,7 @@
 
 static void ndb_print_text_search_key(struct ndb_text_search_key *key)
 {
-	printf("K<'%.*s' %" PRIu64 " %" PRIu64 " note_id:%" PRIu64 ">", key->str_len, key->str,
+	fprintf(stderr,"K<'%.*s' %" PRIu64 " %" PRIu64 " note_id:%" PRIu64 ">", key->str_len, key->str,
 						    key->word_index,
 						    key->timestamp,
 						    key->note_id);
@@ -14,23 +14,11 @@ static void print_hex(unsigned char* data, size_t size) {
 	}
 }
 
-
-static void ndb_print_text_search_result(struct ndb_txn *txn,
-		struct ndb_text_search_result *r)
-{
-	size_t len;
-	struct ndb_note *note;
-
-	ndb_print_text_search_key(&r->key);
-
-	if (!(note = ndb_get_note_by_key(txn, r->key.note_id, &len))) {
-		printf(": note not found");
-		return;
+static void print_hex_stream(FILE *stream, unsigned char* data, size_t size) {
+	size_t i;
+	for (i = 0; i < size; i++) {
+		fprintf(stream, "%02x", data[i]);
 	}
-
-	printf(" ");
-	print_hex(ndb_note_id(note), 32);
-
-	printf("\n%s\n\n---\n", ndb_note_content(note));
 }
+
 
