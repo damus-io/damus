0d5105de0aeeef3b68ca713edcb4e13fdb98c4a1
Swift Coder <scoder1747@gmail.com>
2024-10-23 13:53:59 -0400

    code cleanup, fixed "me" issue for other users

--- damus/Views/QRCodeView.swift
+++ damus/Views/QRCodeView.swift
@@ -120,9 +120,6 @@ struct QRCodeView: View {
         VStack(alignment: .center) {
             let profile_txn = damus_state.profiles.lookup(id: pubkey, txn_name: "qrview-profile")
             let profile = profile_txn?.unsafeUnownedValue
-            let our_profile = profile_txn.flatMap({ ptxn in
-                damus_state.ndb.lookup_profile_with_txn(damus_state.pubkey, txn: ptxn)?.profile
-            })
 
             ProfilePicView(pubkey: pubkey, size: 90.0, highlight: .custom(DamusColors.white, 3.0), profiles: damus_state.profiles, disable_animation: damus_state.settings.disable_animation)
                     .padding(.top, 20)
@@ -153,7 +150,10 @@ struct QRCodeView: View {
 
             Spacer()
             
-            Text("Follow me on Nostr", comment: "Text on QR code view to prompt viewer looking at screen to follow the user.")
+            
+            Text(verbatim: "Follow " +
+                            ((damus_state.pubkey.npub == pubkey.npub) ? "me" : "\(profile?.display_name ?? "")") +
+                             " on Nostr")
                 .font(.system(size: 24, weight: .heavy))
                 .padding(.top, 10)
                 .foregroundColor(.white)
