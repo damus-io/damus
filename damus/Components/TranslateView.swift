212b4785fbf2ba87bc4447dfb1546d3aa513ffef
Terry Yiu <git@tyiu.xyz>
2025-03-01 16:30:25 -0500

    Fix note rendering for those that contain previewable items or leading and trailing whitespaces
    Changelog-Fixed: Fixed note rendering for those that contain previewable items or leading and trailing whitespaces
    Closes: https://github.com/damus-io/damus/issues/2187
    Signed-off-by: Terry Yiu <git@tyiu.xyz>

--- damus/Components/TranslateView.swift
+++ damus/Components/TranslateView.swift
@@ -160,7 +160,7 @@ func translate_note(profiles: Profiles, keypair: Keypair, event: NostrEvent, set
 
     // Render translated note
     let translated_blocks = parse_note_content(content: .content(translated_note, event.tags))
-    let artifacts = render_blocks(blocks: translated_blocks, profiles: profiles)
+    let artifacts = render_blocks(blocks: translated_blocks, profiles: profiles, can_hide_last_previewable_refs: true)
     
     // and cache it
     return .translated(Translated(artifacts: artifacts, language: note_lang))
