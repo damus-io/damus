1fc79c92d3b608650fa4745b27007195a4b2879e
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2025-03-16 21:41:56 +0530

    Implemented Search feature in ConfigSettingsView Closes : https://github.com/damus-io/damus/issues/2838 Changelog-Added : Added a search interface in the configsettings screen Signed-off-by: SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>

--- damus/Views/ConfigView.swift
+++ damus/Views/ConfigView.swift
@@ -18,11 +18,13 @@ struct ConfigView: View {
     @State var delete_account_warning: Bool = false
     @State var confirm_delete_account: Bool = false
     @State var delete_text: String = ""
+    @State private var searchText: String = ""
 
     @ObservedObject var settings: UserSettingsStore
 
     private let DELETE_KEYWORD = "DELETE"
 
+    
     init(state: DamusState) {
         self.state = state
         _settings = ObservedObject(initialValue: state.settings)
@@ -36,86 +38,111 @@ struct ConfigView: View {
         ZStack(alignment: .leading) {
             Form {
                 Section {
-                    NavigationLink(value: Route.KeySettings(keypair: state.keypair)) {
-                        IconLabel(NSLocalizedString("Keys", comment: "Settings section for managing keys"), img_name: "Key", color: .purple)
-                    }
-
-                    NavigationLink(value: Route.AppearanceSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("Appearance and filters", comment: "Section header for text, appearance, and content filter settings"), img_name: "eye", color: .red)
-                    }
-
-                    NavigationLink(value: Route.SearchSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("Search/Universe", comment: "Section header for search/universe settings"), img_name: "search", color: .red)
-                    }
-
-                    NavigationLink(value: Route.NotificationSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("Notifications", comment: "Section header for Damus notifications"), img_name: "notification-bell-on", color: .blue)
-                    }
-
-                    NavigationLink(value: Route.ZapSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("Zaps", comment: "Section header for zap settings"), img_name: "zap.fill", color: .orange)
-                    }
-
-                    NavigationLink(value: Route.TranslationSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("Translation", comment: "Section header for text and appearance settings"), img_name: "globe", color: .green)
-                    }
-                    
-                    NavigationLink(value: Route.ReactionsSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("Reactions", comment: "Section header for reactions settings"), img_name: "shaka.fill", color: .purple)
-                    }
-                    
-                    NavigationLink(value: Route.DeveloperSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("Developer", comment: "Section header for developer settings"), img_name: "magic-stick2.fill", color: DamusColors.adaptableBlack)
-                    }
-                    
-                    NavigationLink(value: Route.FirstAidSettings(settings: settings)) {
-                        IconLabel(NSLocalizedString("First Aid", comment: "Section header for first aid tools and settings"), img_name: "help2", color: .red)
-                    }
-                }
-
-                Section(NSLocalizedString("Sign Out", comment: "Section title for signing out")) {
-                    Button(action: {
-                        if state.keypair.privkey == nil {
-                            logout(state)
-                        } else {
-                            confirm_logout = true
+                    // Keys
+                    if searchText.isEmpty || NSLocalizedString("Keys", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.KeySettings(keypair: state.keypair)) {
+                            IconLabel(NSLocalizedString("Keys", comment: "Settings section for managing keys"), img_name: "Key", color: .purple)
                         }
-                    }, label: {
-                        Label(NSLocalizedString("Sign out", comment: "Sidebar menu label to sign out of the account."), image: "logout")
-                            .foregroundColor(textColor())
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                    })
+                    }
+                    // Appearance and filters
+                    if searchText.isEmpty || NSLocalizedString("Appearance and filters", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.AppearanceSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("Appearance and filters", comment: "Section header for text, appearance, and content filter settings"), img_name: "eye", color: .red)
+                        }
+                    }
+                    // Search/Universe
+                    if searchText.isEmpty || NSLocalizedString("Search Universe", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.SearchSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("Search/Universe", comment: "Section header for search/universe settings"), img_name: "search", color: .red)
+                        }
+                    }
+                    //Notifications
+                    if searchText.isEmpty || NSLocalizedString("Notifications", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.NotificationSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("Notifications", comment: "Section header for Damus notifications"), img_name: "notification-bell-on", color: .blue)
+                        }
+                    }
+                    //Zaps
+                    if searchText.isEmpty || NSLocalizedString("Zaps", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.ZapSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("Zaps", comment: "Section header for zap settings"), img_name: "zap.fill", color: .orange)
+                        }
+                    }
+                    //Translation
+                    if searchText.isEmpty || NSLocalizedString("Translation", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.TranslationSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("Translation", comment: "Section header for text and appearance settings"), img_name: "globe", color: .green)
+                        }
+                    }
+                    //Reactions
+                    if searchText.isEmpty || NSLocalizedString("Reactions", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.ReactionsSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("Reactions", comment: "Section header for reactions settings"), img_name: "shaka.fill", color: .purple)
+                        }
+                    }
+                    //Developer
+                    if searchText.isEmpty || NSLocalizedString("Developer", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.DeveloperSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("Developer", comment: "Section header for developer settings"), img_name: "magic-stick2.fill", color: DamusColors.adaptableBlack)
+                        }
+                    }
+                    //First Aid
+                    if searchText.isEmpty || NSLocalizedString("First Aid", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                        NavigationLink(value: Route.FirstAidSettings(settings: settings)) {
+                            IconLabel(NSLocalizedString("First Aid", comment: "Section header for first aid tools and settings"), img_name: "help2", color: .red)
+                        }
+                    }
                 }
-
-                if state.is_privkey_user {
-                    Section(header: Text("Permanently Delete Account", comment: "Section title for deleting the user")) {
+                //Sign out
+                if searchText.isEmpty || NSLocalizedString("Sign out ", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                    Section(NSLocalizedString("Sign Out", comment: "Section title for signing out")) {
                         Button(action: {
-                            delete_account_warning = true
+                            if state.keypair.privkey == nil {
+                                logout(state)
+                            } else {
+                                confirm_logout = true
+                            }
                         }, label: {
-                            Label(NSLocalizedString("Delete Account", comment: "Button to delete the user's account."), image: "delete")
+                            Label(NSLocalizedString("Sign out", comment: "Sidebar menu label to sign out of the account."), image: "logout")
+                                .foregroundColor(textColor())
                                 .frame(maxWidth: .infinity, alignment: .leading)
-                                .foregroundColor(.red)
                         })
                     }
                 }
-                
-                Section(
-                    header: Text(NSLocalizedString("Version", comment: "Section title for displaying the version number of the Damus app.")),
-                    footer: Text("").padding(.bottom, tabHeight + getSafeAreaBottom())
-                ) {
-                    Text(verbatim: VersionInfo.version)
-                        .contextMenu {
-                            Button {
-                                UIPasteboard.general.string = VersionInfo.version
-                            } label: {
-                                Label(NSLocalizedString("Copy", comment: "Context menu option for copying the version of damus."), image: "copy2")
-                            }
+                // Delete Account
+                if searchText.isEmpty || NSLocalizedString("Delete Account", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                    if state.is_privkey_user {
+                        Section(header: Text("Permanently Delete Account", comment: "Section title for deleting the user")) {
+                            Button(action: {
+                                delete_account_warning = true
+                            }, label: {
+                                Label(NSLocalizedString("Delete Account", comment: "Button to delete the user's account."), image: "delete")
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                    .foregroundColor(.red)
+                            })
                         }
+                    }
+                }
+                if searchText.isEmpty || NSLocalizedString("Version", comment: "Settings section for managing keys").lowercased().contains(searchText.lowercased()){
+                    Section(
+                        header: Text(NSLocalizedString("Version", comment: "Section title for displaying the version number of the Damus app.")),
+                        footer: Text("").padding(.bottom, tabHeight + getSafeAreaBottom())
+                    ) {
+                        Text(verbatim: VersionInfo.version)
+                            .contextMenu {
+                                Button {
+                                    UIPasteboard.general.string = VersionInfo.version
+                                } label: {
+                                    Label(NSLocalizedString("Copy", comment: "Context menu option for copying the version of damus."), image: "copy2")
+                                }
+                            }
+                    }
                 }
             }
         }
         .navigationTitle(NSLocalizedString("Settings", comment: "Navigation title for Settings view."))
         .navigationBarTitleDisplayMode(.large)
+        .searchable(text: $searchText,prompt: "Search within settings")
         .alert(NSLocalizedString("WARNING:\n\nTHIS WILL SIGN AN EVENT THAT DELETES THIS ACCOUNT.\n\nYOU WILL NO LONGER BE ABLE TO LOG INTO DAMUS USING THIS ACCOUNT KEY.\n\n ARE YOU SURE YOU WANT TO CONTINUE?", comment: "Alert for deleting the users account."), isPresented: $delete_account_warning) {
 
             Button(NSLocalizedString("Cancel", comment: "Cancel deleting the user."), role: .cancel) {
