damus-io/damus

commit 22f2aba96979c0f36d2b7165db08635db43774f4

nwc: Wallet Redesign
This PR redesigns the NWC wallet view. A new view is added to introduce zaps to users. The set up wallet view is simplified, with new and existing wallet setup separated.
This also adds new NWC features such as getBalance and listTransactions allowing users to see their balance and previous transactions made.

Changelog-Added: Added view introducing users to Zaps
Changelog-Added: Added new wallet view with balance and transactions list
Changelog-Changed: Improved integration with Nostr Wallet Connect wallets
Closes: https://github.com/damus-io/damus/issues/2900

Signed-off-by: ericholguin <ericholguin@apache.org>
Co-Authored-By: Daniel D’Aquino <daniel@daquino.me>
Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

damus/Views/Wallet/TransactionsView.swift
@@ -1,0 +1,147 @@
+//
+//  TransactionsView.swift
+//  damus
+//
+//  Created by eric on 1/23/25.
+//
+
+import SwiftUI
+
+struct TransactionView: View {
+    
+    let damus_state: DamusState
+    var transaction: WalletConnect.Transaction
+    
+    var body: some View {
+        let txType = transaction.type == "incoming" ? "arrow-bottom-left" : "arrow-top-right"
+        let txColor = transaction.type == "incoming" ? DamusColors.success : Color.gray
+        let txOp = transaction.type == "incoming" ? "+" : "-"
+        let created_at = Date.init(timeIntervalSince1970: TimeInterval(transaction.created_at))
+        let formatter = RelativeDateTimeFormatter()
+        let relativeDate = formatter.localizedString(for: created_at, relativeTo: Date.now)
+        let event = decode_nostr_event_json(transaction.description ?? "")
+        let pubkey = (event?.pubkey ?? ANON_PUBKEY)
+        
+        VStack(alignment: .leading) {
+            HStack(alignment: .center) {
+                ZStack {
+                    ProfilePicView(pubkey: pubkey, size: 45, highlight: .custom(.damusAdaptableBlack, 0.1), profiles: damus_state.profiles, disable_animation: damus_state.settings.disable_animation)
+                    
+                    Image(txType)
+                        .resizable()
+                        .frame(width: 18, height: 18)
+                        .foregroundColor(.white)
+                        .padding(2)
+                        .background(txColor)
+                        .clipShape(Circle())
+                        .overlay(Circle().stroke(Color.damusAdaptableWhite, lineWidth: 1.0))
+                        .padding(.top, 25)
+                        .padding(.leading, 35)
+                }
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    
+                    Text(self.userDisplayName(pubkey: pubkey))
+                        .font(.headline)
+                        .bold()
+                        .foregroundColor(DamusColors.adaptableBlack)
+                    
+                    Text("\(relativeDate)")
+                        .font(.caption)
+                        .foregroundColor(Color.gray)
+                }
+                .padding(.horizontal, 10)
+                
+                Spacer()
+
+                Text("\(txOp) \(transaction.amount/1000) sats")
+                    .font(.headline)
+                    .foregroundColor(txColor)
+                    .bold()
+            }
+            .frame(maxWidth: .infinity, minHeight: 75, alignment: .center)
+            .padding(.horizontal, 10)
+            .background(DamusColors.neutral1)
+            .cornerRadius(10)
+            .overlay(
+                RoundedRectangle(cornerRadius: 10)
+                    .stroke(DamusColors.neutral3, lineWidth: 1)
+            )
+        }
+    }
+    
+    func userDisplayName(pubkey: Pubkey) -> String {
+        let profile_txn = damus_state.profiles.lookup(id: pubkey, txn_name: "txview-profile")
+        let profile = profile_txn?.unsafeUnownedValue
+        
+        if let display_name = profile?.display_name {
+            return display_name
+        } else if let name = profile?.name {
+            return "@" + name
+        } else {
+            return NSLocalizedString("Unknown", comment: "A name label for an unknown user")
+        }
+    }
+    
+}
+
+struct TransactionsView: View {
+    
+    let damus_state: DamusState
+    let transactions: [WalletConnect.Transaction]?
+    var sortedTransactions: [WalletConnect.Transaction]? {
+        transactions?.sorted(by: { $0.created_at > $1.created_at })
+    }
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Latest transactions", comment: "Heading for latest wallet transactions list")
+                .foregroundStyle(DamusColors.neutral6)
+            
+            if let sortedTransactions {
+                if sortedTransactions.isEmpty {
+                    emptyTransactions
+                } else {
+                    ForEach(sortedTransactions, id: \.self) { transaction in
+                        TransactionView(damus_state: damus_state, transaction: transaction)
+                    }
+                }
+            }
+            else {
+                // Make sure we do not show "No transactions yet" to the user when still loading (or when failed to load)
+                // This is important because if we show that when things are not loaded properly, we risk scaring the user into thinking that they have lost funds.
+                emptyTransactions
+                    .redacted(reason: .placeholder)
+                    .shimmer(true)
+            }
+        }
+    }
+    
+    var emptyTransactions: some View {
+        HStack {
+            Text("No transactions yet", comment: "Message shown when no transactions are available")
+                .foregroundStyle(DamusColors.neutral6)
+        }
+        .frame(maxWidth: .infinity, minHeight: 75, alignment: .center)
+        .padding(.horizontal, 10)
+        .background(DamusColors.neutral1)
+        .cornerRadius(10)
+        .overlay(
+            RoundedRectangle(cornerRadius: 10)
+                .stroke(DamusColors.neutral3, lineWidth: 1)
+        )
+    }
+}
+
+struct TransactionsView_Previews: PreviewProvider {
+    static let tds = test_damus_state
+    static let transaction1: WalletConnect.Transaction = WalletConnect.Transaction(type: "incoming", invoice: "", description: "{\"id\":\"7c0999a5870ca3ba0186a29a8650152b555cee29b53b5b8747d8a3798042d01c\",\"pubkey\":\"b8851a06dfd79d48fc325234a15e9a46a32a0982a823b54cdf82514b9b120ba1\",\"created_at\":1736383715,\"kind\":9734,\"tags\":[[\"p\",\"520830c334a3f79f88cac934580d26f91a7832c6b21fb9625690ea2ed81b5626\"],[\"amount\",\"21000\"],[\"e\",\"a25e152a4cd1b3bbc3d22e8e9315d8ea1f35c227b2f212c7cff18abff36fa208\"],[\"relays\",\"wss://nos.lol\",\"wss://nostr.wine\",\"wss://premium.primal.net\",\"wss://relay.damus.io\",\"wss://relay.nostr.band\",\"wss://relay.nostrarabia.com\"]],\"content\":\"🫡 Onward!\",\"sig\":\"e77d16822fa21b9c2e6b580b51c470588052c14aeb222f08f0e735027e366157c8742a6d5cb850780c2bf44ac63d89b048e5cc56dd47a1bfc740a3173e578f4e\"}", description_hash: "", preimage: "", payment_hash: "1234567890", amount: 21000, fees_paid: 0, created_at: 1737736866, expires_at: 0, settled_at: 0)
+    static let transaction2: WalletConnect.Transaction = WalletConnect.Transaction(type: "incoming", invoice: "", description: "", description_hash: "", preimage: "", payment_hash: "123456789033", amount: 100000000, fees_paid: 0, created_at: 1737690090, expires_at: 0, settled_at: 0)
+    static let transaction3: WalletConnect.Transaction = WalletConnect.Transaction(type: "outgoing", invoice: "", description: "", description_hash: "", preimage: "", payment_hash: "123456789042", amount: 303000, fees_paid: 0, created_at: 1737590101, expires_at: 0, settled_at: 0)
+    static let transaction4: WalletConnect.Transaction = WalletConnect.Transaction(type: "incoming", invoice: "", description: "", description_hash: "", preimage: "", payment_hash: "1234567890662", amount: 720000, fees_paid: 0, created_at: 1737090300, expires_at: 0, settled_at: 0)
+    static var test_transactions: [WalletConnect.Transaction] = [transaction1, transaction2, transaction3, transaction4]
+    
+    static var previews: some View {
+        TransactionsView(damus_state: tds, transactions: test_transactions)
+    }
+}

damus/Components/NoteZapButton.swift
@@ -232,7 +232,7 @@ func send_zap(damus_state: DamusState, target: ZapTarget, lnurl: String, is_cust
                 flusher = .once({ pe in
                     // send donation zap when the pending zap is flushed, this allows user to cancel and not send a donation
                     Task { @MainActor in
-                        await send_donation_zap(pool: damus_state.pool, postbox: damus_state.postbox, nwc: nwc_state.url, percent: damus_state.settings.donation_percent, base_msats: amount_msat)
+                        await WalletConnect.send_donation_zap(pool: damus_state.pool, postbox: damus_state.postbox, nwc: nwc_state.url, percent: damus_state.settings.donation_percent, base_msats: amount_msat)
                     }
                 })
             }
@@ -240,7 +240,7 @@ func send_zap(damus_state: DamusState, target: ZapTarget, lnurl: String, is_cust
             // we don't have a delay on one-tap nozaps (since this will be from customize zap view)
             let delay = damus_state.settings.nozaps ? nil : 5.0
 
-            let nwc_req = nwc_pay(url: nwc_state.url, pool: damus_state.pool, post: damus_state.postbox, invoice: inv, delay: delay, on_flush: flusher)
+            let nwc_req = WalletConnect.pay(url: nwc_state.url, pool: damus_state.pool, post: damus_state.postbox, invoice: inv, delay: delay, on_flush: flusher)
 
             guard let nwc_req, case .nwc(let pzap_state) = pending_zap_state else {
                 print("nwc: failed to send nwc request for zapreq \(reqid.reqid)")

damus/ContentView.swift
@@ -367,7 +367,9 @@ struct ContentView: View {
             self.confirm_mute = true
         }
         .onReceive(handle_notify(.attached_wallet)) { nwc in
+            // Ensure to add NWC relay to the pool and connect it.
             try? damus_state.pool.add_relay(.nwc(url: nwc.relay))
+            damus_state.pool.connect(to: [nwc.relay])
 
             // update the lightning address on our profile when we attach a
             // wallet with an associated

damus/Models/HomeModel.swift
@@ -260,7 +260,7 @@ class HomeModel: ContactsDelegate {
             // TODO: Adapt KeychainStorage to StringCodable and instead of parsing to WalletConnectURL every time
             guard let nwc_str = damus_state.settings.nostr_wallet_connect,
                   let nwc = WalletConnectURL(str: nwc_str),
-                  let resp = await FullWalletResponse(from: ev, nwc: nwc) else {
+                  let resp = await WalletConnect.FullWalletResponse(from: ev, nwc: nwc) else {
                 return
             }
 
@@ -274,12 +274,24 @@ class HomeModel: ContactsDelegate {
             
             guard resp.response.error == nil else {
                 print("nwc error: \(resp.response)")
-                nwc_error(zapcache: self.damus_state.zaps, evcache: self.damus_state.events, resp: resp)
+                WalletConnect.handle_error(zapcache: self.damus_state.zaps, evcache: self.damus_state.events, resp: resp)
                 return
             }
             
+            if resp.response.result_type == .list_transactions {
+                Log.info("Received NWC transaction list from %s", for: .nwc, relay.absoluteString)
+                damus_state.wallet.handle_nwc_response(response: resp)
+                return
+            }
+            
+            if resp.response.result_type == .get_balance {
+                Log.info("Received NWC balance information from %s", for: .nwc, relay.absoluteString)
+                damus_state.wallet.handle_nwc_response(response: resp)
+                return
+            }
+
             print("nwc success: \(resp.response.result.debugDescription) [\(relay)]")
-            nwc_success(state: self.damus_state, resp: resp)
+            WalletConnect.handle_zap_success(state: self.damus_state, resp: resp)
         }
     }
 
@@ -453,7 +465,7 @@ class HomeModel: ContactsDelegate {
                    let nwc = WalletConnectURL(str: nwc_str),
                    nwc.relay == relay_id
                 {
-                    subscribe_to_nwc(url: nwc, pool: pool)
+                    WalletConnect.subscribe(url: nwc, pool: pool)
                 }
             case .error(let merr):
                 let desc = String(describing: merr)

damus/Models/WalletModel.swift
@@ -13,10 +13,17 @@ enum WalletConnectState {
     case none
 }
 
+/// Models and manages the user's NWC wallet based on the app's settings
 class WalletModel: ObservableObject {
     var settings: UserSettingsStore
     private(set) var previous_state: WalletConnectState
     var initial_percent: Int
+    /// The wallet's balance, in sats.
+    /// Starts with `nil` to signify it is not loaded yet
+    @Published private(set) var balance: Int64? = nil
+    /// The list of NWC transactions made in the wallet
+    /// Starts with `nil` to signify it is not loaded yet
+    @Published private(set) var transactions: [WalletConnect.Transaction]? = nil
     
     @Published private(set) var connect_state: WalletConnectState
     
@@ -60,5 +67,28 @@ class WalletModel: ObservableObject {
         notify(.attached_wallet(nwc))
         self.connect_state = .existing(nwc)
         self.previous_state = .existing(nwc)
+    }
+
+    /// Handles an NWC response event and updates the model.
+    /// 
+    /// This takes a response received from the NWC relay and updates the internal state of this model.
+    /// 
+    /// - Parameter response: The NWC response received from the network
+    func handle_nwc_response(response: WalletConnect.FullWalletResponse) {
+        switch response.response.result {
+        case .get_balance(let balanceResp):
+            self.balance = balanceResp.balance / 1000
+        case .none:
+            return
+        case .some(.pay_invoice(_)):
+            return
+        case .list_transactions(let transactionsResp):
+            self.transactions = transactionsResp.transactions
+        }
+    }
+    
+    func resetWalletStateInformation() {
+        self.transactions = nil
+        self.balance = nil
     }
 }

damus/NIP04/NIP04.swift
@@ -1,0 +1,55 @@
+//
+//  NIP04.swift
+//  damus
+//
+//  Created by Daniel D’Aquino on 2025-03-10.
+//
+import Foundation
+
+/// Functions and utilities for the NIP-04 spec
+struct NIP04 {}
+
+extension NIP04 {
+    /// Encrypts a message using NIP-04.
+    static func encrypt_message(message: String, privkey: Privkey, to_pk: Pubkey, encoding: EncEncoding = .base64) -> String? {
+        let iv = random_bytes(count: 16).bytes
+        guard let shared_sec = get_shared_secret(privkey: privkey, pubkey: to_pk) else {
+            return nil
+        }
+        let utf8_message = Data(message.utf8).bytes
+        guard let enc_message = aes_encrypt(data: utf8_message, iv: iv, shared_sec: shared_sec) else {
+            return nil
+        }
+        
+        switch encoding {
+        case .base64:
+            return encode_dm_base64(content: enc_message.bytes, iv: iv)
+        case .bech32:
+            return encode_dm_bech32(content: enc_message.bytes, iv: iv)
+        }
+        
+    }
+    
+    /// Creates an event with encrypted `contents` field, using NIP-04
+    static func create_encrypted_event(_ message: String, to_pk: Pubkey, tags: [[String]], keypair: FullKeypair, created_at: UInt32, kind: UInt32) -> NostrEvent? {
+        let privkey = keypair.privkey
+        
+        guard let enc_content = encrypt_message(message: message, privkey: privkey, to_pk: to_pk) else {
+            return nil
+        }
+        
+        return NostrEvent(content: enc_content, keypair: keypair.to_keypair(), kind: kind, tags: tags, createdAt: created_at)
+    }
+    
+    /// Creates a NIP-04 style direct message event
+    static func create_dm(_ message: String, to_pk: Pubkey, tags: [[String]], keypair: Keypair, created_at: UInt32? = nil) -> NostrEvent?
+    {
+        let created = created_at ?? UInt32(Date().timeIntervalSince1970)
+        
+        guard let keypair = keypair.to_full() else {
+            return nil
+        }
+        
+        return create_encrypted_event(message, to_pk: to_pk, tags: tags, keypair: keypair, created_at: created, kind: 4)
+    }
+}

damus/Nostr/NostrEvent+.swift
@@ -68,7 +68,7 @@ func make_private_zap_request_event(identity: FullKeypair, enc_key: FullKeypair,
     
     guard let note = NostrEvent(content: message, keypair: identity.to_keypair(), kind: 9733, tags: tags),
           let note_json = encode_json(note),
-          let enc = encrypt_message(message: note_json, privkey: enc_key.privkey, to_pk: target.pubkey, encoding: .bech32)
+          let enc = NIP04.encrypt_message(message: note_json, privkey: enc_key.privkey, to_pk: target.pubkey, encoding: .bech32)
     else {
         return nil
     }

commit 1cb311cc2c3b89490dc78c9ba38bc6584ab88ab9

Fix localization issues in TransactionsView
Fixes: 22f2aba96979 ("Fix localization issues in TransactionsView")
Changelog-Fixed: Fix localization issues in TransactionsView
Signed-off-by: Terry Yiu <git@tyiu.xyz>

damus/Views/Wallet/TransactionsView.swift
@@ -13,9 +13,10 @@ struct TransactionView: View {
     var transaction: WalletConnect.Transaction
     
     var body: some View {
-        let txType = transaction.type == "incoming" ? "arrow-bottom-left" : "arrow-top-right"
-        let txColor = transaction.type == "incoming" ? DamusColors.success : Color.gray
-        let txOp = transaction.type == "incoming" ? "+" : "-"
+        let isIncomingTransaction = transaction.type == "incoming"
+        let txType = isIncomingTransaction ? "arrow-bottom-left" : "arrow-top-right"
+        let txColor = isIncomingTransaction ? DamusColors.success : Color.gray
+        let txOp = isIncomingTransaction ? "+" : "-"
         let created_at = Date.init(timeIntervalSince1970: TimeInterval(transaction.created_at))
         let formatter = RelativeDateTimeFormatter()
         let relativeDate = formatter.localizedString(for: created_at, relativeTo: Date.now)
@@ -46,7 +47,7 @@ struct TransactionView: View {
                         .bold()
                         .foregroundColor(DamusColors.adaptableBlack)
                     
-                    Text("\(relativeDate)")
+                    Text(relativeDate)
                         .font(.caption)
                         .foregroundColor(Color.gray)
                 }
@@ -54,7 +55,7 @@ struct TransactionView: View {
                 
                 Spacer()
 
-                Text("\(txOp) \(transaction.amount/1000) sats")
+                Text(verbatim: "\(txOp) \(format_msats(transaction.amount))")
                     .font(.headline)
                     .foregroundColor(txColor)
                     .bold()