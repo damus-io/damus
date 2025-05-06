damus-io/damus

commit 67f0e3d2967b3d436d431e3660436db1634e228c

Add disclaimer to Coinos button
Changelog-Changed: Added disclaimer to clarify that Coinos is a third-party service
Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

damus/Views/Wallet/ConnectWalletView.swift
@@ -148,8 +148,14 @@ struct ConnectWalletView: View {
             
             Spacer()
             
-            CoinosButton() {
-                self.show_coinos_options = true
+            VStack(spacing: 5) {
+                CoinosButton() {
+                    self.show_coinos_options = true
+                }
+                Text("Coinos is a service operated by a third-party. We have no access to your Coinos wallet.", comment: "Small caption with a disclaimer that Damus does not own or have access to Coinos wallets, Coinos is a third-party service.")
+                    .font(.caption)
+                    .foregroundStyle(.secondary)
+                    .multilineTextAlignment(.center)
             }
             .padding()
         }

commit 806555c00f4ca57f533eade07001584cb40e0d6c

Improve Coinos button disclaimer
Closes: https://github.com/damus-io/damus/issues/3000
Fixes: 67f0e3d2967b3d436d431e3660436db1634e228c
Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

damus/Views/Wallet/ConnectWalletView.swift
@@ -152,7 +152,7 @@ struct ConnectWalletView: View {
                 CoinosButton() {
                     self.show_coinos_options = true
                 }
-                Text("Coinos is a service operated by a third-party. We have no access to your Coinos wallet.", comment: "Small caption with a disclaimer that Damus does not own or have access to Coinos wallets, Coinos is a third-party service.")
+                Text("Coinos is a service operated by a third-party. The Damus team has no access to your wallet.", comment: "Small caption with a disclaimer that Damus does not own or have access to Coinos wallets, Coinos is a third-party service.")
                     .font(.caption)
                     .foregroundStyle(.secondary)
                     .multilineTextAlignment(.center)