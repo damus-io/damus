9eeb00c8974174f0d8c5b8f420c90abbcbc1d239
Terry Yiu <git@tyiu.xyz>
2025-06-04 00:14:34 -0400

    Add tip in threads to inform users what trusted network means
    Changelog-Added: Added tip in threads to inform users what trusted network means
    Signed-off-by: Terry Yiu <git@tyiu.xyz>

--- damus/Models/UserSettingsStore.swift
+++ damus/Models/UserSettingsStore.swift
@@ -131,6 +131,9 @@ class UserSettingsStore: ObservableObject {
     @Setting(key: "show_trusted_replies_first", default_value: true)
     var show_trusted_replies_first: Bool
 
+    @Setting(key: "reset_tips_on_launch", default_value: false)
+    var reset_tips_on_launch: Bool
+
     @Setting(key: "hide_nsfw_tagged_content", default_value: false)
     var hide_nsfw_tagged_content: Bool
     
