2b5172c63d25162b2adfbe58c0b6b5a248993d50
SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>
2025-06-14 19:37:39 +0530

    Rename "Universe" to "Search" Changelog-Changed: Renamed "Universe" to "Search" in UI screen Signed-off-by: SanjaySiddharth <mjsanjaysiddharth1999@gmail.com>

--- damus/ContentView.swift
+++ damus/ContentView.swift
@@ -1086,7 +1086,7 @@ func timeline_name(_ timeline: Timeline?) -> String {
     case .notifications:
         return NSLocalizedString("Notifications", comment: "Toolbar label for Notifications view.")
     case .search:
-        return NSLocalizedString("Universe 🛸", comment: "Toolbar label for the universal view where notes from all connected relay servers appear.")
+        return NSLocalizedString("Search 🛸", comment: "Toolbar label for the universal view where notes from all connected relay servers appear.")
     case .dms:
         return NSLocalizedString("DMs", comment: "Toolbar label for DMs view, where DM is the English abbreviation for Direct Message.")
     }
