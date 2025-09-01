ab22206093ed3c283d56778f4b011c8fa2b757e5
Daniel D’Aquino <daniel@daquino.me>
2025-09-01 15:04:29 -0700

    Fix broken Follow Pack timeline
    Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

--- damus/Features/Search/Models/SearchHomeModel.swift
+++ damus/Features/Search/Models/SearchHomeModel.swift
@@ -47,7 +47,11 @@ class SearchHomeModel: ObservableObject {
         let to_relays = damus_state.nostrNetwork.ourRelayDescriptors
             .map { $0.url }
             .filter { !damus_state.relay_filters.is_filtered(timeline: .search, relay_id: $0) }
-        outerLoop: for await item in damus_state.nostrNetwork.reader.subscribe(filters: [get_base_filter()], to: to_relays) {
+        
+        var follow_list_filter = NostrFilter(kinds: [.follow_list])
+        follow_list_filter.until = UInt32(Date.now.timeIntervalSince1970)
+        
+        outerLoop: for await item in damus_state.nostrNetwork.reader.subscribe(filters: [get_base_filter(), follow_list_filter], to: to_relays) {
             switch item {
             case .event(let borrow):
                 var event: NostrEvent? = nil
