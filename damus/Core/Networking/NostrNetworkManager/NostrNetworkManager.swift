damus-io/damus

commit b562b930cccc1e50f4916758f31ade0840314682

Prevent new NostrDB streaming tasks from opening when NostrDB has begun to close
We have mechanisms in place to close NostrDB streams when the database needs to
close; however, there is a short time window where those streams are
closing down but the database still has its "open" property set to `true`,
which means that new NostrDB streams may open. If that happens, those
new streams will still be active when NostrDB gets closed down,
potentially causing memory crashes.

This was found by inspecting several crash logs and noticing that:
- most of the `ndb.close` calls are coming from the general
  backgrounding task (not the last resort backgrounding task),
  where all old tasks are guaranteed to have closed (we wait for all of
  them to close before proceeding to closing NostrDB).
- the stack traces of the crashed threads show that, in most cases, the
  stream crashes while they are in the query stage (which means that
  those must have been very recently opened).

The issue was mitigated by signalling that NostrDB has closed (without
actually closing it) before cancelling any streaming tasks and officially
closing NostrDB. This way, new NostrDB streaming tasks will notice that
the database is closed and will wait for it to reopen.

No changelog entry is needed as this issue was introduced after our last public
release.

Changelog-None
Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

damus/Core/Networking/NostrNetworkManager/NostrNetworkManager.swift
@@ -59,9 +59,19 @@ class NostrNetworkManager {
         await self.pool.disconnect()
     }
     
-    func handleAppBackgroundRequest() async {
+    func handleAppBackgroundRequest(beforeClosingNdb operationBeforeClosingNdb: (() async -> Void)? = nil) async {
+        // Mark NDB as closed without actually closing it, to avoid new tasks from using NostrDB
+        // self.delegate.ndb.markClosed()
         await self.reader.cancelAllTasks()
         await self.pool.cleanQueuedRequestForSessionEnd()
+        await operationBeforeClosingNdb?()
+        self.delegate.ndb.close()
+    }
+    
+    func handleAppForegroundRequest() async {
+        self.delegate.ndb.reopen()
+        // Pinging the network will automatically reconnect any dead websocket connections
+        await self.ping()
     }
     
     func close() async {

damus/ContentView.swift
@@ -538,9 +538,7 @@ struct ContentView: View {
                     Log.debug("App background signal handling: App being backgrounded", for: .app_lifecycle)
                     let startTime = CFAbsoluteTimeGetCurrent()
                     await damus_state.nostrNetwork.handleAppBackgroundRequest()  // Close ndb streaming tasks before closing ndb to avoid memory errors
-                    Log.debug("App background signal handling: Nostr network closed after %.2f seconds", for: .app_lifecycle, CFAbsoluteTimeGetCurrent() - startTime)
-                    damus_state.ndb.close()
-                    Log.debug("App background signal handling: Ndb closed after %.2f seconds", for: .app_lifecycle, CFAbsoluteTimeGetCurrent() - startTime)
+                    Log.debug("App background signal handling: Nostr network and Ndb closed after %.2f seconds", for: .app_lifecycle, CFAbsoluteTimeGetCurrent() - startTime)
                     this_app.endBackgroundTask(bgTask)
                 }
                 break
@@ -552,9 +550,7 @@ struct ContentView: View {
                 Task {
                     await damusClosingTask?.value  // Wait for the closing task to finish before reopening things, to avoid race conditions
                     damusClosingTask = nil
-                    damus_state.ndb.reopen()
-                    // Pinging the network will automatically reconnect any dead websocket connections
-                    await damus_state.nostrNetwork.ping()
+                    await damus_state.nostrNetwork.handleAppForegroundRequest()
                 }
             @unknown default:
                 break
@@ -1143,7 +1139,6 @@ extension LossyLocalNotification {
         }
     }
 }
-
 
 func logout(_ state: DamusState?)
 {

nostrdb/Ndb.swift
@@ -207,6 +207,12 @@ class Ndb {
         self.callbackHandler = Ndb.CallbackHandler()
     }
     
+    /// Mark NostrDB as "closed" without actually closing it.
+    /// Useful when shutting down tasks that use NostrDB while avoiding new tasks from using it.
+    func markClosed() {
+        self.closed = true
+    }
+    
     func close() {
         guard !self.is_closed else { return }
         self.closed = true

commit f5e5da25eb09fdc044ddc98b2129defd8812105a

Remove accidental code comment
This commit removes an accidentally placed code comment. Behavior has
been tested during the original work related to that commit.

Fixes: b562b930cccc1e50f4916758f31ade0840314682
Signed-off-by: Daniel D’Aquino <daniel@daquino.me>

damus/Core/Networking/NostrNetworkManager/NostrNetworkManager.swift
@@ -61,7 +61,7 @@ class NostrNetworkManager {
     
     func handleAppBackgroundRequest(beforeClosingNdb operationBeforeClosingNdb: (() async -> Void)? = nil) async {
         // Mark NDB as closed without actually closing it, to avoid new tasks from using NostrDB
-        // self.delegate.ndb.markClosed()
+        self.delegate.ndb.markClosed()
         await self.reader.cancelAllTasks()
         await self.pool.cleanQueuedRequestForSessionEnd()
         await operationBeforeClosingNdb?()